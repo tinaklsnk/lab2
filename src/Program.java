204c66c737f02cfe154c5d30ed29cde79370ffd6
tinaklsnk <valentaine29012002@gmail.com>
2021-02-28 21:04:50 +0200

    created new interface

--- src/Program.java
+++ src/Program.java
@@ -1,2 +1,52 @@
 public class Program {
+    public static void main(String[] args) {
+
+    }
+}
+
+interface Bird {
+    void fly();
+    void sit();
+}
+
+class Predator implements Bird{
+    @Override
+    public void fly() {
+        System.out.println(" Хижа літає");
+    }
+
+    @Override
+    public void sit() {
+        System.out.println("Хижа сіла");
+    }
+}
+
+class NonPredatory implements Bird {
+    @Override
+    public void fly() {
+        System.out.println("Нехижа літає");
+    }
+
+    @Override
+    public void sit() {
+        System.out.println("Нехижа сіла");
+    }
+}
+
+ abstract class Factory {
+    abstract void getBird();
+}
+
+class FactoryProducer {
+    public static Bird getFactory() {
+        int r=0;
+        switch (r) {
+            case 0:
+                return new Predator();
+            case 1:
+                return new NonPredatory();
+            default:
+                return null;
+        }
+    }
 }
